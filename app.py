1f54513b00186ff5138acee424135f9494557b4a
lbanh777 <97317094+lbanh777@users.noreply.github.com>
2024-01-11 12:55:23 -0800

    Add CNN model files

--- app.py
+++ app.py
@@ -2,26 +2,39 @@ import streamlit as st
 import numpy as np
 import cv2
 from joblib import load
+import tensorflow as tf
+
 
 st.title("TITLE")
 st.header("Description of the app")
 
-model = load("model.joblib")
+ALPHABET = ['A', 'B', 'C', 'D', 'E',
+              'F', 'G', 'H', 'I', 'J',
+              'K', 'L', 'M', 'N', 'O',
+              'P', 'Q', 'R', 'S', 'T',
+              'U', 'V', 'W', 'X', 'Y', 'Z']
+
+model = tf.keras.models.load_model("best_cnn_model.h5")
 st.write ("Model uploaded!")
 
 f = st.camera_input("Take a picture")
 
+height, width = 48, 48
+
 if f is not None:
     file_bytes = np.asarray(bytearray(f.read()), dtype=np.uint8)
-    img = cv2.imdecode(file_bytes, cv2.IMREAD_GRAYSCALE)
-    img = img[0:396, 154:550] #crop picture to (396, 396) i.e. a square
-    
+    image = cv2.imdecode(file_bytes, 1)
+    image = image[0:396, 154:550]
+
     # Prepare the image
-    img = cv2.resize(img, (64, 64))
-    img_reshaped = cv2.resize(img, (64 * 64, 1))
+    resized = cv2.resize(image, (48, 48), interpolation=cv2.INTER_LANCZOS4)
+    gray = np.mean(resized, axis=-1)
+
+    normalized = gray/255
+    model_input = np.expand_dims(normalized,0)
 
     #Predict the output
-    y_pred = model.predict(img_reshaped)
+    scores = model.predict(model_input).flatten()
 
-    # Print results and plot score
-    st.write(f"Predicted Letter: ", y_pred)
+    # Print results
+    st.write(f"Translation: {ALPHABET[scores.argmax()]}")
